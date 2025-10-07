6f4842d36927f57b9d239e0b4d82be48635cb2bd
Rehanon-Mackenzie <fairywings38@hotmail.com>
2025-10-07 13:11:19 +0100

    add ordered high scores list

--- assets/scripts/highscores.js
+++ assets/scripts/highscores.js
@@ -1,8 +1,8 @@
 const highScoresList = document.getElementById('highScoresList');
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-console.log(
-    highScores.map(score => {
-return`<li class="high-score">${score.name} - ${score.score}</li>`;
+highScoresList.innerHTML =
+highScores.map(score => {
+    return(`<li class="high-score">Detective ${score.playerName} - ${score.score}</li>`)
 })
-);
+.join("");
